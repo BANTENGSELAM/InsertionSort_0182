aa37c378fdf70921009168f556996aae84feb461
“BANTENGSELAM” <pedjuangjava@gmail.com>
2026-03-13 15:17:37 +0700

    prosedur input

--- Insertion_sort.cpp
+++ Insertion_sort.cpp
@@ -4,3 +4,26 @@ using namespace std;
 
 int arr[20];    //membuat array dengan panjang data 20
 int n;          //membuat variable inputan n
+
+void input (){  //prosedur input
+    while (true){
+        cout << "Masukan Jumlah Data Pada array : " ;
+        cin >> n;
+        
+        if (n <=20){
+            break;
+        }
+        else {
+            cout << "\nArray Tidak Boleh Lebih dari 20\n";
+        }
+    }
+    cout << endl;
+    cout << "=================================================";
+    cout << "==============Masukan Element Array==============";
+    cout << "=================================================" << endl;
+    
+    for (int i = 0; i < n; i++){
+        cout << "Data ke-" << (i+1) << ": ";
+        cin >> arr[i];
+    }
+}
